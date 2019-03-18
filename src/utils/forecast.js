a0a1e5ae4bf6544c2e588fccad1195ce69267432
pavelklos <pavelkloscz@gmail.com>
2019-03-18 06:30:47 +0100

    Setup app for Heroku 3

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,15 +2,21 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/156ca666fcabd614ccf775e892b98bef/' + latitude + ',' + longitude + '?units=si'
-    // const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
-
+ 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            var date = new Date(body.currently.time * 1000);
+            var localDate = date.toLocaleString('cs-CZ', { timeZone: body.timezone })
+            // console.log('timezone:', body.timezone)
+            // console.log('offset:', body.offset)
+            // console.log('time:', body.currently.time)
+            // console.log(localDate);
+
+            callback(undefined, 'Local time: ' +  localDate + '. ' + body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
